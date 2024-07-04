107f28d98208fefc57229b5227041c9a545b67be
Daniel Janik <daniel.janik@bluesoft.com>
2024-07-04 15:46:11 +0200

    lib type

--- projects/event-lib-mf/src/lib/event-lib-mf.service.ts
+++ projects/event-lib-mf/src/lib/event-lib-mf.service.ts
@@ -1,20 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Subject, Observable, filter, map } from 'rxjs';
 
+export enum EventType {
+  UPDATEUSER = 'updateUser',
+  ADDTOCART = 'addToCart',
+}
+interface UpdateUserEvent {
+  event: EventType.ADDTOCART;
+  data: { productId: string; userName: string };
+}
+
+interface AddToCartEvent {
+  event: EventType.UPDATEUSER;
+  data: { userId: string; quantity: number };
+}
+
+export type EventServiceType = UpdateUserEvent | AddToCartEvent;
+
 @Injectable({
   providedIn: 'root',
 })
-export class EventService {
-  private subject = new Subject<{ event: string; data: any }>();
+export class EventService<EventServiceType> {
+  private subject: Subject<{ event: EventType; data: any }> = new Subject<{
+    event: EventType;
+    data: any;
+  }>();
 
-  on<T>(event: string): Observable<T> {
+  on(event: EventType): Observable<EventServiceType> {
     return this.subject.asObservable().pipe(
       filter((eventData) => eventData.event === event),
-      map((eventData) => eventData.data as T)
+      map((eventData) => eventData.data as EventServiceType)
     );
   }
 
-  emit<T>(event: string, data: T) {
+  emit(event: EventType, data: EventServiceType) {
     this.subject.next({ event, data });
   }
 }
